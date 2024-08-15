8ed896226ff59e155d78a294939b1a55892b4570
shunny <shunnyd3v22@gmail.com>
2024-08-15 18:56:00 -0500

    Actualizar cat.js para la configuración en Vercel

--- api/cat.js
+++ api/cat.js
@@ -1,14 +1,10 @@
-const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
-const app = express();
 
 // Leer el archivo JSON una vez al inicio
 const imagesData = JSON.parse(fs.readFileSync(path.join(__dirname, '../images/cat.json'), 'utf-8'));
 
-// Endpoint para obtener una imagen aleatoria de gatos
-app.get('/cat', (req, res) => {
+module.exports = (req, res) => {
   const catImages = imagesData.cats;
   if (catImages.length === 0) {
     return res.status(404).json({ error: 'No se encontraron imágenes de gatos' });
@@ -16,6 +12,4 @@ app.get('/cat', (req, res) => {
 
   const randomCatImage = catImages[Math.floor(Math.random() * catImages.length)];
   res.json({ url: randomCatImage });
-});
-
-module.exports = app;
+};
