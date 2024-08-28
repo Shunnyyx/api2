c4fc097fa000d1526bed75ed64e3c7d61700ec20
shunny <shunnyd3v22@gmail.com>
2024-08-27 19:48:45 -0500

    Primer commit

---
+++ api/fox.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (req, res) => {
+  // Leer el archivo JSON una vez al inicio
+  const imagesPath = path.join(__dirname, '../images/fox.json');
+  
+  fs.readFile(imagesPath, 'utf-8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error al leer el archivo de imágenes' });
+    }
+
+    const imagesData = JSON.parse(data);
+    const foxImages = imagesData.foxes;
+
+    if (!foxImages || foxImages.length === 0) {
+      return res.status(404).json({ error: 'No se encontraron imágenes de zorros' });
+    }
+
+    const randomFoxImage = foxImages[Math.floor(Math.random() * foxImages.length)];
+    res.json({ url: randomFoxImage });
+  });
+};
