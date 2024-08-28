17660270f6a42e5923a0edfbc514a9e4d2b07645
shunny <shunnyd3v22@gmail.com>
2024-08-27 19:56:29 -0500

    Añadido endpoint para GIFs de abrazos de anime

--- api/fox.js
+++ api/fox.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  // Leer el archivo JSON una vez al inicio
+  // Leer el archivo JSON de zorros una vez al inicio
   const imagesPath = path.join(__dirname, '../images/fox.json');
   
   fs.readFile(imagesPath, 'utf-8', (err, data) => {
     if (err) {
-      return res.status(500).json({ error: 'Error al leer el archivo de imágenes' });
+      return res.status(500).json({ error: 'Error al leer el archivo de imágenes de zorros' });
     }
 
     const imagesData = JSON.parse(data);
-    const foxImages = imagesData.foxes;
+    const foxImages = imagesData.fox;
 
     if (!foxImages || foxImages.length === 0) {
       return res.status(404).json({ error: 'No se encontraron imágenes de zorros' });
