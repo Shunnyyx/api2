3e05c0c6a73ea8f0b38667ed80d9dd44b373f182
shunny <shunnyd3v22@gmail.com>
2024-08-29 21:30:12 -0500

    Añadido endpoint para GIFs de abrazos de anime

--- api/morse.js
+++ api/morse.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const action = document.querySelector('input[name="action"]:checked').value;
         const message = document.querySelector('#message').value;
 
-        fetch('http://localhost:3000/api/morse', {
+        fetch('https://api.aiko.top/api/morse', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
