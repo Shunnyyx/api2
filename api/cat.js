95e28786e12e12adf4300a1c111f21b48813c301
shunny <shunnyd3v22@gmail.com>
2024-09-16 18:00:36 -0500

    Añadido endpoint para GIFs de abrazos de anime

--- api/cat.js
+++ api/cat.js
@@ -1,23 +1,41 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = (req, res) => {
-  // Leer el archivo JSON una vez al inicio
-  const imagesPath = path.join(__dirname, '../images/cat.json');
-  
-  fs.readFile(imagesPath, 'utf-8', (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: 'Error al leer el archivo de imágenes' });
+const validKeys = [];
+
+// Cargar todas las claves válidas al inicio
+function loadValidKeys() {
+  const files = ['db1.json', 'db2.json'];
+  files.forEach(file => {
+    const filePath = path.join(__dirname, '../database', file);
+    try {
+      const data = fs.readFileSync(filePath, 'utf-8');
+      const jsonData = JSON.parse(data);
+      if (jsonData.keys) {
+        validKeys.push(...jsonData.keys);
+      }
+    } catch (error) {
+      console.error(`Error reading file ${file}: ${error.message}`);
     }
-
-    const imagesData = JSON.parse(data);
-    const catImages = imagesData.cats;
-
-    if (!catImages || catImages.length === 0) {
-      return res.status(404).json({ error: 'No se encontraron imágenes de gatos' });
-    }
-
-    const randomCatImage = catImages[Math.floor(Math.random() * catImages.length)];
-    res.json({ url: randomCatImage });
   });
+}
+
+loadValidKeys();
+
+module.exports = (req, res) => {
+  const apiKey = req.headers['api-key']; // Asume que la clave se envía en los encabezados
+
+  if (!apiKey) {
+    return res.status(400).json({ error: 'API key is required' });
+  }
+
+  if (validKeys.includes(apiKey)) {
+    // La clave es válida, responder con éxito
+    res.json({ message: 'API key is valid' });
+  } else {
+    // La clave no es válida, responder con error
+    res.status(403).json({
+      error: 'Invalid API key. Get a free key at https://discord.gg/vYHWyDd4Bp'
+    });
+  }
 };
