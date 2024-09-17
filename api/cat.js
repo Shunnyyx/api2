c25968226b727cb32d8c91ae91f1c59fa9698a24
shunny <shunnyd3v22@gmail.com>
2024-09-16 19:10:22 -0500

    Añadido endpoint para GIFs de abrazos de anime

--- api/cat.js
+++ api/cat.js
@@ -1,47 +1,29 @@
-const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const router = express.Router();
-const cache = require('memory-cache');
 
-const imagesPath = path.join(__dirname, '../images/cat.json');
-const cacheKey = 'catImages';
-const cacheDuration = 60000; // 1 minuto
-
-// Función para cargar imágenes de gatos en memoria
-const loadCatImages = () => {
+module.exports = (req, res) => {
+  // Leer el archivo JSON de gatos
+  const imagesPath = path.join(__dirname, '../images/cat.json');
+  
   fs.readFile(imagesPath, 'utf-8', (err, data) => {
     if (err) {
-      console.error('Error al leer el archivo de imágenes de gatos:', err);
-      return;
+      return res.status(500).json({ error: 'Error al leer el archivo de imágenes de gatos' });
     }
 
+    let imagesData;
     try {
-      const imagesData = JSON.parse(data);
-      const catImages = imagesData.cats || [];
-      if (catImages.length === 0) {
-        console.warn('El archivo JSON no contiene imágenes de gatos');
-      }
-      cache.put(cacheKey, catImages, cacheDuration);
+      imagesData = JSON.parse(data);
     } catch (parseErr) {
-      console.error('Error al analizar el archivo de imágenes de gatos:', parseErr);
+      return res.status(500).json({ error: 'Error al analizar el archivo de imágenes de gatos' });
     }
+
+    const catImages = imagesData.cats;
+
+    if (!catImages || catImages.length === 0) {
+      return res.status(404).json({ error: 'No se encontraron imágenes de gatos' });
+    }
+
+    const randomCatImage = catImages[Math.floor(Math.random() * catImages.length)];
+    res.json({ url: randomCatImage });
   });
 };
-
-// Cargar imágenes al iniciar el módulo
-loadCatImages();
-
-// Endpoint para obtener imágenes de gatos
-router.get('/', (req, res) => {
-  const catImages = cache.get(cacheKey);
-
-  if (!catImages || catImages.length === 0) {
-    return res.status(500).json({ error: 'No se han cargado imágenes de gatos' });
-  }
-
-  const randomCatImage = catImages[Math.floor(Math.random() * catImages.length)];
-  res.json({ url: randomCatImage });
-});
-
-module.exports = router;
